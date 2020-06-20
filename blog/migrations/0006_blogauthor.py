e80a055ba186cae00b0ffc9817eefb1be43bf996
zainabade <zainabadelopo@gmail.com>
2020-06-20 12:37:22 +0100

    fixed dependencies

--- blog/migrations/0006_blogauthor.py
+++ blog/migrations/0006_blogauthor.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailimages', '0022_uploadedimage'),
         ('blog', '0005_auto_20200617_1604'),
     ]
 
